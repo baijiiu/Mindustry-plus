747b3d97debd3724b3ac9c1d88f5d319d62869d4
Administrator <p@>
2025-08-02 15:04:08 +0800

    1

--- src/mindustryPlus/MindustryPlusMain.java
+++ src/mindustryPlus/MindustryPlusMain.java
@@ -1,17 +1,14 @@
-package example;
+package mindustryPlus;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MindustryPlusMain extends Mod{
 
-    public ExampleJavaMod(){
+    public MindustryPlusMain(){
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
@@ -21,7 +18,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("mindustry-plus-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
